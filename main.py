60c7bfa07acaa396005c70578840bfa5d87d9f66
antonincms <9219052+antonincms@users.noreply.github.com>
2022-06-18 01:43:34 +0200

    :children_crossing: - Simplified usage of PictureProcessor

--- main.py
+++ main.py
@@ -2,15 +2,15 @@
 
 import argparse
 import cProfile
-import sys
 from os import system
 
 import cgp.cgp_utilies
-import mario_emu.picture_processing
 from controller import get_top, post_top
 from mario_emu.emulator import Emulator
 
 # Hyper parameters
+from mario_emu.picture_processor import PictureProcessor
+
 POPULATION_SIZE = 100
 GENOME_SIZE = 1024
 NB_BREED = 5  # Number of children generated from know-good parents
@@ -20,9 +20,6 @@ NB_MUTATIONS = 20  # Number of mutation per genome applied from a parent to a ch
 SAVE_EVERY = 5  # Number of states between saves
 KEEP = 10  # Number of good children to pass to the next gen
 
-# Program parameters
-image_processor = mario_emu.picture_processing.PictureAnalyser()
-
 
 def learn(mpi_comm=None, debug=False, render=False, save_name=None, host_name=None):
     if mpi_comm:
@@ -30,7 +27,7 @@ def learn(mpi_comm=None, debug=False, render=False, save_name=None, host_name=No
     else:
         rank = 0
 
-    cfg = cgp.cgp_utilies.GenomeConfig(image_processor.get_dim(), 7, GENOME_SIZE)
+    cfg = cgp.cgp_utilies.GenomeConfig(PictureProcessor.get_dim(), 7, GENOME_SIZE)
 
     if save_name is None:
         pop = cgp.cgp_utilies.Population(cfg, POPULATION_SIZE, KEEP)
@@ -48,7 +45,7 @@ def learn(mpi_comm=None, debug=False, render=False, save_name=None, host_name=No
             if rank == 0:
                 print("Testing generation {}...".format(i))
 
-        Emulator.eval_population(pop, image_processor, render=render, debug=debug)
+        Emulator.eval_population(pop, render=render, debug=debug)
         pop.keep_bests()
 
         print("Best scores were {}...".format(pop.list_scores[:5]))
@@ -59,9 +56,7 @@ def learn(mpi_comm=None, debug=False, render=False, save_name=None, host_name=No
                     post_top(host_name, pop.serialize()), pop.genome_config
                 )
                 if debug:
-                    print(
-                        "Saving on server {}, new state is {}...".format(host_name, pop)
-                    )
+                    print("Saving on server {}, new state is {}...".format(host_name, pop))
             elif mpi_comm:
                 shared = [
                     cgp.cgp_utilies.deserialize_population(p, pop.genome_config)
@@ -81,9 +76,9 @@ def learn(mpi_comm=None, debug=False, render=False, save_name=None, host_name=No
 
 def profile_run():
     for i in range(2):
-        cfg = cgp.cgp_utilies.GenomeConfig(image_processor.get_dim(), 7, 1000)
+        cfg = cgp.cgp_utilies.GenomeConfig(PictureProcessor.get_dim(), 7, 1000)
         pop = cgp.cgp_utilies.Population(cfg, 10)
-        Emulator.eval_population(pop, image_processor)
+        Emulator.eval_population(pop)
         pop.next_gen()
 
 
@@ -92,7 +87,7 @@ def profile():
 
 
 def visualize(save_name):
-    cfg = cgp.cgp_utilies.GenomeConfig(image_processor.get_dim(), 7, GENOME_SIZE)
+    cfg = cgp.cgp_utilies.GenomeConfig(PictureProcessor.get_dim(), 7, GENOME_SIZE)
     pop = cgp.cgp_utilies.load_population(cfg, save_name)
     for (i, gen) in enumerate(pop.list_genomes):
         gen.compute_used_node()
@@ -101,21 +96,32 @@ def visualize(save_name):
         with open(infile + ".gv", "w") as f:
             f.write("digraph gen{} {{\n".format(i))
             for j in range(gen.conf.inp):
-                f.write('    {} [label="{}"];\n'.format(j, "input " + str(j)))
-                f.write('    {} -> {};\n'.format("Input", j))
+                f.write('\t{} [label="{}"];\n'.format(j, "input " + str(j)))
+                f.write("\t{} -> {};\n".format("Input", j))
             for j in range(gen.conf.inp, gen.conf.inp + gen.conf.node):
                 if gen.used_node[j]:
-                    f.write('    {} [label="{}"];\n'.format(j, "{}: {}".format(j, gen.genotype[
-                        j - gen.conf.inp].fn_name())))
+                    f.write(
+                        '\t{} [label="{}"];\n'.format(
+                            j, "{}: {}".format(j, gen.genotype[j - gen.conf.inp].fn_name())
+                        )
+                    )
                     for p in gen.genotype[j - gen.conf.inp].preds():
-                        f.write('    {} -> {};\n'.format(p, j))
-            for j in range(gen.conf.inp + gen.conf.node, gen.conf.inp + gen.conf.node + gen.conf.out):
-                f.write('    {} [label="{}"];\n'.format(j, "output {}: {}".format(j - gen.conf.node - gen.conf.inp,
-                                                                                  gen.genotype[
-                                                                                      j - gen.conf.inp].fn_name())))
-                f.write('    {} -> {};\n'.format(j, "Decision"))
+                        f.write("\t{} -> {};\n".format(p, j))
+            for j in range(
+                gen.conf.inp + gen.conf.node, gen.conf.inp + gen.conf.node + gen.conf.out
+            ):
+                f.write(
+                    '\t{} [label="{}"];\n'.format(
+                        j,
+                        "output {}: {}".format(
+                            j - gen.conf.node - gen.conf.inp,
+                            gen.genotype[j - gen.conf.inp].fn_name(),
+                        ),
+                    )
+                )
+                f.write("\t{} -> {};\n".format(j, "Decision"))
                 for p in gen.genotype[j - gen.conf.inp].preds():
-                    f.write('    {} -> {};\n'.format(p, j))
+                    f.write("\t{} -> {};\n".format(p, j))
 
             f.write("}")
         system("dot -Tpng {}.gv -o {}.png".format(infile, infile))
@@ -123,25 +129,17 @@ def visualize(save_name):
 
 def main():
     # PARSER CONFIGURATION
-    parser = argparse.ArgumentParser(
-        description="Cartesian Genetic Program playing Mario Bros :3"
-    )
+    parser = argparse.ArgumentParser(description="Cartesian Genetic Program playing Mario Bros :3")
     parser.add_argument("-l", "--load", help="Load a save file.")
-    parser.add_argument(
-        "-d", "--debug", help="Display debugging messages.", action="store_true"
-    )
+    parser.add_argument("-d", "--debug", help="Display debugging messages.", action="store_true")
     parser.add_argument(
         "-r", "--render", help="Display screen during playing.", action="store_true"
     )
     parser.add_argument(
-        "-c",
-        "--collector",
-        help="Use a remote server to share results between workers.")
+        "-c", "--collector", help="Use a remote server to share results between workers."
+    )
     parser.add_argument(
-        "-m",
-        "--mpi",
-        help="Start the program in distributed with MPI.",
-        action="store_true",
+        "-m", "--mpi", help="Start the program in distributed with MPI.", action="store_true"
     )
     parser.add_argument(
         "-p",
@@ -152,7 +150,7 @@ def main():
     parser.add_argument(
         "-v",
         "--visualize",
-        help="Generate graphs for given save name, should NOT be used with other options."
+        help="Generate graphs for given save name, should NOT be used with other options.",
     )
 
     # PARSING
@@ -172,6 +170,7 @@ def main():
         # Configuring variables
         if args.mpi:
             from mpi4py import MPI
+
             comm = MPI.COMM_WORLD
         else:
             comm = None
--- mario_emu/emulator.py
+++ mario_emu/emulator.py
@@ -3,22 +3,22 @@ from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
 
 from cgp.cgp_model import Population, Genome
+from mario_emu.picture_processor import PictureProcessor
 
 
 class Emulator:
-    def __init__(self, picture_processor):
+    def __init__(self):
         self.env = gym_super_mario_bros.make("SuperMarioBros-2-1-v2")
         self.env = BinarySpaceToDiscreteSpaceEnv(self.env, SIMPLE_MOVEMENT)
-        self.picture_processor = picture_processor
 
-    def _make_it_play(self, g: Genome, render: bool) -> int:
+    def _make_it_play(self, g: Genome, render: bool) -> float:
         observation = self.env.reset()
         total_reward = 0.0
         stuck_score = 0
         for i in range(1, 10000):
             if render:
                 self.env.render()
-            ob_flat = self.picture_processor.process(observation)
+            ob_flat = PictureProcessor.process(observation)
             decision = g.evaluate(ob_flat)
             action = decision.argmax()
             # action = self.env.action_space.sample()
@@ -36,15 +36,11 @@ class Emulator:
         return total_reward
 
     @staticmethod
-    def eval_population(p: Population, processor, render=False, debug=False):
-        e = Emulator(processor)
+    def eval_population(p: Population, render=False, debug=False):
+        e = Emulator()
         for i in range(len(p.list_genomes)):
             if p.list_scores[i] is None:
                 p.list_scores[i] = e._make_it_play(p.list_genomes[i], render)
             if debug:
-                print(
-                    "Genome {} got reward {}".format(
-                        p.list_genomes[i], p.list_scores[i]
-                    )
-                )
+                print("Genome {} got reward {}".format(p.list_genomes[i], p.list_scores[i]))
         # Get 5 (or keep) best genome by sorting by mark the list, slice it and reconstruct it

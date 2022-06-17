5f00d91c55db1f67134d28de148974e79ecf40b6
antonincms <9219052+antonincms@users.noreply.github.com>
2022-06-18 01:43:34 +0200

    Added a new picture processor

--- projet/PictureProcessing.py
+++ projet/PictureProcessing.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 
 
@@ -15,15 +16,31 @@ class PictureFlattener:
         return np.array(screen).flatten()
 
 
-class PictureBasicAnalyser:
-    map: {(int, int, int): int}
+class PictureReducer:
 
     @classmethod
     def get_dim(cls) -> int:
-        NotImplemented
+        return 15 * 14
+
+    def process(self, screen: [[[]]], debug=False) -> []:
+        screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+        screen = cv2.resize(screen, (15, 14))
+        if debug:
+            cv2.imshow('main', screen)
+            cv2.waitKey(1)
+        screen = np.reshape(screen, (15, 14))
+        return screen.flatten()
+
+        # 1 PIX = 16*16
+
+
+class PictureAnalyser:
+    # {(104, 136, 252): 0, (252, 252, 252): 1, (252, 160, 68): 2, (228, 92, 16): 3,
+    # (0, 0, 0): 4, (184, 248, 24): 5, (240, 208, 176): 6, (248, 56, 0): 7}
+
+    @classmethod
+    def get_dim(cls) -> int:
+        return 15 * 14
 
     def process(self, screen: [[[]]]) -> []:
-        CompressedPicture = [[[]]]
-        # 1 PIX = 16*16
-        # {(104, 136, 252): 0, (252, 252, 252): 1, (252, 160, 68): 2, (228, 92, 16): 3, (0, 0, 0): 4, (184, 248, 24): 5, (240, 208, 176): 6, (248, 56, 0): 7}
-        NotImplemented
+        return NotImplemented

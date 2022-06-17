9595b038251ce93a0904fa5149c15269a2932e55
antonincms <9219052+antonincms@users.noreply.github.com>
2022-06-18 01:43:34 +0200

    Linting, some hintings, some refactoring

--- projet/ProtoCGP/CgpModel.py
+++ projet/ProtoCGP/CgpModel.py
@@ -1,22 +1,34 @@
 import json
 from random import randint
 
-_default_func=[
-    lambda x,y: x+y,
-    lambda x,y: x-y,
-    lambda x,y: x*y,
-    lambda x,y: x/y
+_default_func = [
+    lambda x, y: x + y,
+    lambda x, y: x - y,
+    lambda x, y: x * y,
+    lambda x, y: x / y
 ]
 
-class Neurone:
-    def __init__(self, conf, col_id):
-        self.col_id = col_id
-        self.conf = conf
-        self.pred1 = randint(0, conf.inp + (col_id)*conf.row -1)
-        self.pred2 = randint(0, conf.inp + (col_id)*conf.row -1)
-        self.func = conf.func[randint(0, len(conf.func) -1)]
 
-    def evaluate(self, genome):
+class GenomeConfig:
+    def __init__(self, inp, out, row=1, col=1, func=None):
+        if func is None:
+            func = _default_func
+        self.inp = inp
+        self.out = out
+        self.row = row
+        self.col = col
+        self.func = func
+
+
+class Neurone:
+    def __init__(self, genome_config: GenomeConfig, col_id: int):
+        self.col_id = col_id
+        self.conf = genome_config
+        self.pred1 = randint(0, genome_config.inp + col_id * genome_config.row - 1)
+        self.pred2 = randint(0, genome_config.inp + col_id * genome_config.row - 1)
+        self.func = genome_config.func[randint(0, len(genome_config.func) - 1)]
+
+    def evaluate(self, genome: []):
         if self.pred1 < self.conf.inp:
             val1 = genome[self.pred1]
         else:
@@ -28,72 +40,71 @@ class Neurone:
         return self.func(val1, val2)
 
     def mutate(self):
-        rand = randint(0,2)
+        rand = randint(0, 2)
         if rand == 0:
-            self.pred1 = randint(0, self.conf.inp + (self.col_id)*self.conf.row -1)
-        elif rand==1:
-            self.pred2 = randint(0, self.conf.inp + (self.col_id)*self.conf.row -1)
+            self.pred1 = randint(0, self.conf.inp + self.col_id * self.conf.row - 1)
+        elif rand == 1:
+            self.pred2 = randint(0, self.conf.inp + self.col_id * self.conf.row - 1)
         else:
-            self.func = self.conf.func[randint(0, len(self.conf.func) -1)]
+            self.func = self.conf.func[randint(0, len(self.conf.func) - 1)]
 
-
-class GenomeConfig:
-    def __init__(self, inp, out, row = 1, col = 1, func = _default_func):
-        self.inp = inp
-        self.out = out
-        self.row = row
-        self.col = col
-        self.func = func
 
 class Genome:
-    def __init__(self, conf):
-        self.conf = conf
-        self.genotype = [0] * conf.inp
-        for c in range(conf.col):
-            self.genotype += [neurone(conf, c) for _ in range(conf.row)]
-        self.genotype += [randint(0, conf.inp + conf.row*conf.col -1) for _ in range(conf.out)]
+    def __init__(self, genome_config: GenomeConfig):
+        self.conf = genome_config
+        self.genotype = [0] * genome_config.inp
+        for c in range(genome_config.col):
+            self.genotype += [Neurone(genome_config, c) for _ in range(genome_config.row)]
+        self.genotype += [randint(0, genome_config.inp + genome_config.row * genome_config.col - 1) for _ in
+                          range(genome_config.out)]
 
-    def evaluate(self,inp):
+    def evaluate(self, inp: []) -> []:
         for i in range(self.conf.inp):
             self.genotype[i] = inp[i]
         res = [0] * self.conf.out
         for i in range(self.conf.out):
-            j = i + self.conf.inp + self.conf.row*self.conf.col
+            j = i + self.conf.inp + self.conf.row * self.conf.col
             if self.genotype[j] < self.conf.inp:
                 res[i] = self.genotype[self.genotype[j]]
             else:
                 res[i] = self.genotype[self.genotype[j]].evaluate(self.genotype)
         return res
-            
-    def mutate(self, nb_mutation):
+
+    def mutate(self, nb_mutation: int):
         for _ in range(nb_mutation):
-            i = randint(self.conf.inp, len(self.genotype) -1)
-            if i < self.conf.inp + self.conf.row*self.conf.col:
+            i = randint(self.conf.inp, len(self.genotype) - 1)
+            if i < self.conf.inp + self.conf.row * self.conf.col:
                 self.genotype[i].mutate()
             else:
-                self.genotype[i] = randint(0, self.conf.inp + self.conf.row*self.conf.col -1)
+                self.genotype[i] = randint(0, self.conf.inp + self.conf.row * self.conf.col - 1)
 
-    def evaluate(self, inputs: []) -> []:
-        pass
+    @staticmethod
+    def serialize() -> str:
+        NotImplemented
 
-    @classmethod
-    def serialize(cls):
-        pass
 
-    @classmethod
-    def deserialize(cls, e):
-        pass
+def deserialize_genome(s: str) -> Genome:
+    pass
 
 
 class Population(object):
     list_genomes = []
     genome_config = GenomeConfig
 
-    def __init__(self, genome_config : GenomeConfig, size = 50):
+    def __init__(self, genome_config: GenomeConfig, size=50):
         self.genome_config = genome_config
         for i in range(size):
             g = Genome(genome_config)
             self.list_genomes.append(g)
+
+    def make_them_play(self, nb_runs=5, keep=5):
+        # Do N runs
+
+        # get fitness
+
+        # get M better elem
+
+        pass
 
     def serialize(self) -> str:
         tmp_list = []
@@ -101,14 +112,16 @@ class Population(object):
             tmp_list.append(g.serialize())
         return json.dumps(self.list_genomes)
 
-    def make_them_play(self, nb_runs = 5, keep = 5):
-        pass
 
-    @classmethod
-    def deserialize(cls, str, genome_config) :
-        tmp_list = json.loads(str)
-        new_pop = Population(genome_config)
-        for e in tmp_list:
-            g = Genome.deserialize(e)
-            new_pop.list_genomes.append(g)
-        return new_pop
+def deserialize_population(s: str, genome_config: GenomeConfig) -> Population:
+    tmp_list = json.loads(s)
+    new_pop = Population(genome_config)
+    for e in tmp_list:
+        g = deserialize_genome(e)
+        new_pop.list_genomes.append(g)
+    return new_pop
+
+
+def generate_population_from(bests: Genome):
+    # give birth bests.size - pop_size from bests winner by mutation
+    pass
--- projet/ProtoCGP/Main.py
+++ projet/ProtoCGP/Main.py
@@ -2,7 +2,7 @@ import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
 
-from ProtoCGP.CgpModel import Population
+from ProtoCGP.CgpModel import Population, generate_population_from, GenomeConfig
 
 NB_GENS = 10000
 POP_SIZE = 50
@@ -16,21 +16,10 @@ class Game:
 
 
 def main():
-    pop = Population(input_size=10000, pop_size=POP_SIZE)
+    cfg = GenomeConfig(23, 32)  # TO TUNE
+    pop = Population(cfg, size=POP_SIZE)
     for i in range(NB_GENS):
-        bests = pop.make_them_play(keep=5)
-        pop = Population.generate_from(bests)
-        if (i % SAVE_EVERY == 0):
-            pop.save("save_"+str(i))
-
-    # Do N runs
-
-    # get fitness
-
-    # get M better elem
-
-    # give birth N-M from N winner by mutation
-
-    # loop
-
-    # save every X run
+        bests = pop.make_them_play()
+        pop = generate_population_from(bests)
+        if i % SAVE_EVERY == 0:
+            pop.save("save_" + str(i))
